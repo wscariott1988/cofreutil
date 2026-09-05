574b1cfe1cb9b5f57c1d3bd7f6458703120cc6ea
Willian Scariott <contato@grupows.com>
2026-09-04 21:51:43 -0300

    fix: adiciona site no astro.config e move headers para server.headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://cofreutil.com.br',
   output: 'static',
   integrations: [react(), tailwind()],
-  headers: {
-    'Cross-Origin-Opener-Policy': 'same-origin',
-    'Cross-Origin-Embedder-Policy': 'require-corp',
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
   vite: {
     optimizeDeps: {
